fe52471c369f0ff5863fb88a6f5c3c570b82c710
Chris Mendez <cmendez20@utexas.edu>
2020-04-22 01:51:53 -0500

    added modals to japan ramen map

--- src/js/main.js
+++ src/js/main.js
@@ -22,3 +22,52 @@ window.addEventListener('keydown' /* or keyup, see what feels better when you te
 //     scrollRight();
 //   }
 // });
+
+// modal 
+const open_sapporo = document.getElementById('sapporo-circle');
+const modal_container = document.getElementById('modal_sapporo');
+const close = document.getElementById('close');
+
+open_sapporo.addEventListener('click', () => {
+  modal_container.classList.add('show');
+});
+
+close.addEventListener('click', () => {
+  modal_container.classList.remove('show');
+});
+
+const open_tokyo = document.getElementById('tokyo-circle');
+const modal_toyko = document.getElementById('modal_tokyo');
+const close_tokyo = document.getElementById('close_tokyo');
+
+open_tokyo.addEventListener('click', () => {
+  modal_tokyo.classList.add('show-tokyo');
+});
+
+close_tokyo.addEventListener('click', () => {
+  modal_tokyo.classList.remove('show-tokyo');
+});
+
+const open_kitakata = document.getElementById('kitakata-circle');
+const modal_kitakata = document.getElementById('modal_kitakata');
+const close_kitakata = document.getElementById('close_kitakata');
+
+open_kitakata.addEventListener('click', () => {
+  modal_kitakata.classList.add('show-kitakata');
+});
+
+close_kitakata.addEventListener('click', () => {
+  modal_kitakata.classList.remove('show-kitakata');
+});
+
+const open_hakata = document.getElementById('hakata-circle');
+const modal_hakata = document.getElementById('modal_hakata');
+const close_hakata = document.getElementById('close_hakata');
+
+open_hakata.addEventListener('click', () => {
+  modal_hakata.classList.add('show-hakata');
+});
+
+close_hakata.addEventListener('click', () => {
+  modal_hakata.classList.remove('show-hakata');
+});
