84c7d9f4bbae9068f320ddf57da16f4537eb7f1a
Chris Mendez <cmendez20@utexas.edu>
2020-05-10 22:07:57 -0500

    pushing to branch master in case I break something.

--- src/js/main.js
+++ src/js/main.js
@@ -24,7 +24,7 @@ window.addEventListener('keydown' /* or keyup, see what feels better when you te
 // });
 
 // modal 
-const open_sapporo = document.getElementById('sapporo-circle');
+const open_sapporo = document.getElementById('sapporo-section');
 const modal_container = document.getElementById('modal_sapporo');
 const close = document.getElementById('close');
 
@@ -36,7 +36,7 @@ close.addEventListener('click', () => {
   modal_container.classList.remove('show');
 });
 
-const open_tokyo = document.getElementById('tokyo-circle');
+const open_tokyo = document.getElementById('tokyo-section');
 const modal_toyko = document.getElementById('modal_tokyo');
 const close_tokyo = document.getElementById('close_tokyo');
 
@@ -48,7 +48,7 @@ close_tokyo.addEventListener('click', () => {
   modal_tokyo.classList.remove('show-tokyo');
 });
 
-const open_kitakata = document.getElementById('kitakata-circle');
+const open_kitakata = document.getElementById('kitakata-section');
 const modal_kitakata = document.getElementById('modal_kitakata');
 const close_kitakata = document.getElementById('close_kitakata');
 
@@ -60,7 +60,7 @@ close_kitakata.addEventListener('click', () => {
   modal_kitakata.classList.remove('show-kitakata');
 });
 
-const open_hakata = document.getElementById('hakata-circle');
+const open_hakata = document.getElementById('hakata-section');
 const modal_hakata = document.getElementById('modal_hakata');
 const close_hakata = document.getElementById('close_hakata');
 
